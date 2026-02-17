37c80d25c84e67166b6250ec3eef89940d3baf02
nahalashyjas-del <nahalashyjas@gmail.com>
2026-02-17 16:55:24 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import './index.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    //backgr dropout
+    <div style={{background:"#e8fa25", color:'white', height:"100vh",justifyItems:'center',alignContent:'center'}}>
+      
+      <div style ={{background:"blue",height:50, width:100,zIndex:100,alignContent:'center'}}>
+offerloop
+      </div>
+
+    </div>
+
+  )
+}
+
+export default App
